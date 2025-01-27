6ddaafc271b44f44eb64bee73d6a928c70bfc294
Joe Young <musicjoeyoung@gmail.com>
2025-01-27 13:10:32 -0500

    console message; width style

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,14 @@ interface EmailFormData {
   email: string
 }
 
+console.log(
+  '%c🌐 Want a stunning website? 🚀 %cReach out to%c Joe Young %cjoseph.m.young2@gmail.com',
+  'color: #51355a; font-size: 18px; font-weight: bold; text-shadow: 2px 2px 4px rgba(81, 53, 90, 0.5);',
+  'color: #3498db; font-size: 18px; font-weight: bold; text-decoration: underline;',
+  'color: #e74c3c; font-size: 18px; font-weight: bold;',
+  'color: #e67e22; font-size: 18px; font-weight: bold;'
+);
+
 const App = () => {
   const [cards, setCards] = useState<Card[]>([])
   const [formData, setFormData] = useState<CardFormData>({
