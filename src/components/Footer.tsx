85b328f9b03c59baa9be726a1ce118f89b408c03
Joe Young <musicjoeyoung@gmail.com>
2025-02-10 12:27:17 -0500

    link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            <p className="footer__copyright">© Valentine's Day Cards - {new Date().getFullYear()} Joseph Young</p>
+            <p className="footer__copyright">© Valentine's Day Cards - {new Date().getFullYear()} <a href="https://josephmyoung.com" target="_blank">Joseph Young</a></p>
             <p className="footer__support">Want to support me? Click the link below!</p>
             <a href="https://www.buymeacoffee.com/josephmyoung" target="_blank">
                 <img className="footer__coffee" src="https://cdn.buymeacoffee.com/buttons/v2/default-violet.png" alt="Buy Me A Coffee" />
